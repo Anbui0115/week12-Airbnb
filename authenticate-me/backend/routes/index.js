1eaa2f875f754b75af86c038d8f20293270f07a6
An Bui <dobui0115@gmail.com>
2022-07-27 14:18:33 -0700

    Add an API router

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
@@ -9,6 +16,7 @@ const router = express.Router();
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
+  console.log(csrfToken)
   res.cookie("XSRF-TOKEN", csrfToken);
   res.status(200).json({
     'XSRF-Token': csrfToken
