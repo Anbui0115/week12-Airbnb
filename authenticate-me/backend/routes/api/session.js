d7d4d629f0292056b4b72fbfedf05fcff1f939b7
An Bui <dobui0115@gmail.com>
2022-07-29 10:00:10 -0700

    Add User logout backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -6,7 +6,7 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-// Log in
+//------------------ Log in------------------
 router.post(
   '/',
   async (req, res, next) => {
@@ -27,6 +27,16 @@ router.post(
     return res.json({
       user
     });
+  }
+);
+
+
+// ----------------Log out------------------
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
   }
 );
 
