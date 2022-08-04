875aa752403b72c140e61025693bc9e192b6cee3
An Bui <dobui0115@gmail.com>
2022-08-04 10:17:17 -0700

    created booking.js

---
+++ backend/routes/api/booking.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+  authenticate,
+} = require("../../utils/auth");
+const { User, Spot, Image, Review, Booking } = require("../../db/models");
+const { Op } = require("sequelize");
+const router = express.Router();
+const { Sequelize } = require("sequelize");
+//start phase 5
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .withMessage("Review is required"),
+  check("stars").exists({ checkFalsy: true }).withMessage("Rating is required"),
+  handleValidationErrors,
+];
+const validateSpot = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .isDecimal()
+    .withMessage("Latitude is not valid"),
+  check("lng")
+    .isDecimal()
+    .withMessage("Longitude is not valid"),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day is required"),
+  handleValidationErrors,
+];
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
